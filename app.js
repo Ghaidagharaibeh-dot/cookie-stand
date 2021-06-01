0eb39efe14239dfce3456452e5c336fda0f06e1a
Ghaidagharaibeh-dot <ghedaghraibeh@yahoo.com>
2021-06-01 21:57:03 +0300

    creat lab 08

--- app.js
+++ app.js
@@ -9,60 +9,20 @@ let worksHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2p
 let createT = document.getElementById('mmmm')
 let tableElm = document.createElement('table')
 createT.appendChild(tableElm)
-let trElm1 = document.createElement('tr');
-tableElm.appendChild(trElm1);
-let thElm0 = document.createElement('th');
-trElm1.appendChild(thElm0);
-thElm0.textContent = '';
-let thElm1 = document.createElement('th');
-trElm1.appendChild(thElm1);
-thElm1.textContent = '6:00am';
-let thElm2 = document.createElement('th');
-trElm1.appendChild(thElm2);
-thElm2.textContent = '7:00am';
-let thElm3 = document.createElement('th');
-trElm1.appendChild(thElm3);
-thElm3.textContent = '8:00am';
-let thElm4 = document.createElement('th');
-trElm1.appendChild(thElm4);
-thElm4.textContent = '9:00am';
-let thElm5 = document.createElement('th');
-trElm1.appendChild(thElm5);
-thElm5.textContent = '10:00am';
-let thElm6 = document.createElement('th');
-trElm1.appendChild(thElm6);
-thElm6.textContent = '11:00am';
-let thElm7 = document.createElement('th');
-trElm1.appendChild(thElm7);
-thElm7.textContent = '12:00am';
-let thElm8 = document.createElement('th');
-trElm1.appendChild(thElm8);
-thElm8.textContent = '1:00pm';
-let thElm9 = document.createElement('th');
-trElm1.appendChild(thElm9);
-thElm9.textContent = '2:00pm';
-let thElm10 = document.createElement('th');
-trElm1.appendChild(thElm10);
-thElm10.textContent = '2:00pm';
-let thElm11 = document.createElement('th');
-trElm1.appendChild(thElm11);
-thElm11.textContent = '3:00pm';
-let thElm12 = document.createElement('th');
-trElm1.appendChild(thElm12);
-thElm12.textContent = '4:00pm';
-let thElm13 = document.createElement('th');
-trElm1.appendChild(thElm13);
-thElm13.textContent = '5:00pm';
-let thElm14 = document.createElement('th');
-trElm1.appendChild(thElm14);
-thElm14.textContent = '6:00pm';
-let thElm15 = document.createElement('th');
-trElm1.appendChild(thElm15);
-thElm15.textContent = '7:00pm';
-let thElm16 = document.createElement('th');
-trElm1.appendChild(thElm16);
-thElm15.textContent = 'Daily Location Total';
 
+let thElm0 = document.createElement('th');
+tableElm.appendChild(thElm0);
+thElm0.textContent = 'mmmmmm';
+for (let i = 0; i <= worksHours.length; i++) {
+    let thElm2 = document.createElement('th');
+    tableElm.appendChild(thElm2);
+    thElm2.textContent = worksHours[i];
+}
+let thElm16 = document.createElement('th');
+tableElm.appendChild(thElm16);
+thElm16.textContent = 'Daily Location Total';
+
+let arrOfObject =[];
 
 function SalmonCookies(loactionName, minCust, maxCust, avgCookieSale) {
     this.loactionName = loactionName;
@@ -70,60 +30,81 @@ function SalmonCookies(loactionName, minCust, maxCust, avgCookieSale) {
     this.maxCust = maxCust;
     this.avgCookieSale = avgCookieSale;
 
-    this.total = 0;
+
     this.cusNum = [];
     this.ammOfPurches = [];
-    
+    this.total = 0;
+    arrOfObject.push(this);
     console.log(this);
 }
 
-SalmonCookies.prototype.funCustomerNum = function () {
+SalmonCookies.prototype.funforCustomerNum = function () {
     for (let l = 0; l <= worksHours.length; l++) {
 
         this.cusNum.push(getRandomInt(this.minCust, this.maxCust));
-       
-}
+
+    }
 }
 SalmonCookies.prototype.funammOfPurches = function () {
     for (let i = 0; i <= worksHours.length; i++) {
         this.ammOfPurches.push(this.cusNum[i] * Math.ceil(this.avgCookieSale));
         this.total = this.total + this.ammOfPurches[i];
-    }
 
+    }
+return
 
 }
 
 SalmonCookies.prototype.render = function () {
 
-    let trElm2 = document.createElement('tr');
-    tableElm.appendChild(trElm2);
-    let tdElm1 = document.createElement('td');
-    trElm2.appendChild(tdElm1);
-    tdElm1.textContent = this.loactionName;
-}
-   
+    let trElm = document.createElement('tr');
+    tableElm.appendChild(trElm);
 
-let seattle = new SalmonCookies("Seattle", 23, 65, 6.3);
-let tokyo = new SalmonCookies("Tokyo", 3, 24, 1.2);
-let dubai = new SalmonCookies("Dubai", 11, 38, 3.7);
-let paris = new SalmonCookies("Paris", 20, 38, 2.3);
-let lima = new SalmonCookies("Lima", 2, 16, 4.6);
-let resultoftotal=new SalmonCookies("totals");
-seattle.funCustomerNum();
-seattle.funammOfPurches();
-tokyo.funCustomerNum();
-tokyo.funammOfPurches();
-dubai.funCustomerNum();
-paris.funCustomerNum();
-lima.funCustomerNum();
-dubai.funammOfPurches();
-paris.funammOfPurches();
-lima.funammOfPurches();
-resultoftotal.funCustomerNum();
-resultoftotal.funammOfPurches();
-seattle.render();
-tokyo.render();
-dubai.render();
-paris.render();
-lima.render();
-resultoftotal.render(); 
+    let thFrow = document.createElement('th');
+    trElm.appendChild(thFrow);
+    thFrow.textContent = this.loactionName;
+
+
+    for (let i = 1; i <= this.ammOfPurches.length; i++) {
+        let tdElm = document.createElement('td');
+        trElm.appendChild(tdElm);
+        tdElm.textContent = this.ammOfPurches[i];
+    }
+    let lasrrow = document.createElement('th');
+    trElm.appendChild(lasrrow);
+    lasrrow.textContent = this.total;
+    return
+}
+
+
+let seattle = new SalmonCookies("Seattle", 23, 65, 6.3, 0, [], []);
+let tokyo = new SalmonCookies("Tokyo", 3, 24, 1.2, 0, [], []);
+let dubai = new SalmonCookies("Dubai", 11, 38, 3.7, 0, [], []);
+let paris = new SalmonCookies("Paris", 20, 38, 2.3, 0, [], []);
+let lima = new SalmonCookies("Lima", 2, 16, 4.6, 0, [], []);
+
+
+for (let i = 0; i <= arrOfObject.length; i++) {
+   arrOfObject[i].funforCustomerNum();
+    arrOfObject[i].funammOfPurches();
+    arrOfObject[i].render();
+}
+
+function tabelfoot (){
+    let footerRow=document.createElement('tr');
+    tableElm.appendChild(footerRow);
+    let contac=Document.createElement('th');
+    footerRow.appendChild(contac);
+    contac.textContent= 'Totals';
+    for(let i=0;i<=worksHours.length;i++){
+        let sum=0;
+        for(let j=0;j<worksHours.length;j++){
+            sum+=arrOfObject[j].ammOfPurches[j];
+        }
+        td=Document.createElement('td');
+        lasrrow.appendChild(td);
+        td.textContent=sum;
+     //   sum=seattle.ammOfPurches[i]+
+    }
+    
+}
